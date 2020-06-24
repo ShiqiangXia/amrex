a7ecf40bec6e4543efbb0b980fe46b6bb79f556d
Shiqiang Xia <xiaxx268@umn.edu>
2020-06-23 19:46:47 -0500

    right side kernel works but not left side kernel

--- Tutorials/DG/Hyperbolic_Advection/main.cpp
+++ Tutorials/DG/Hyperbolic_Advection/main.cpp
@@ -195,7 +195,7 @@ amrex::Print() << "#############################################################
         pp_dG.get("time_p", inputs.dG.time_p);
 
         // choose quadrature basedn on DG polynomial k
-        inputs.dG.space_q = std::max(inputs.dG.phi_space_p+2, inputs.dG.space_p+4);
+        inputs.dG.space_q = std::max(inputs.dG.phi_space_p+2, inputs.dG.space_p+3);
 
         pp_dG.query("use_slope_limiter", inputs.dG.use_slope_limiter);
         if      (inputs.dG.use_slope_limiter == "true")  inputs.dG.use_slope_limiter_flag = true;
